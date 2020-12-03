d7231544a585f52169ecb5721184ebde5fa6de18
Shareena <smdelacruz28@yahoo.com>
2020-12-03 23:19:03 +0800

    restart google course

--- 8kyu/array_madness.py
+++ 8kyu/array_madness.py
@@ -6,7 +6,11 @@ def array_madness(a,b):
     side_b = [i**2 for i in b]
     return True if side_a > side_b  else False
     # print(side_a)
-    # print(side_b)
+    # print(sgitide_b)
 
 
 array_madness([4, 5, 6], [1, 2, 3]) #True
+
+
+
+print(((10 >= 5*2) and (10 <= 5*2)))
--- googleCourse/wk4_dictionary_forloop_wardrobe.py
+++ googleCourse/wk4_dictionary_forloop_wardrobe.py
@@ -2,3 +2,6 @@ wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
 for clothes_type,colors in wardrobe.items():
     for color in colors:
         print("{} {}".format(color, clothes_type))
+
+
+print("big" > "small")
