42d3f33dfa232dfa9fe5c822bfe08bb83bc89d1b
Shareena <smdelacruz28@yahoo.com>
2021-02-16 01:06:50 +0800

    20210216 1am

--- 7kyu/char_code_calculations.py
+++ 7kyu/char_code_calculations.py
@@ -1,16 +1,45 @@
-def calc(x):
-    print(ord(i) for i in x)
-    total1 = sum(ord(i) for i in x)
-    total2 = []
-    for i in x:
-        ascii_int = ord(i)
-        ascii_str = str(ascii_int)
-        if '7' in str(ascii_str):
-            ascii_str = ascii_str.replace('7', '1')
-        total2.append(int(ascii_str))
-        print(total2)
-    print(total1 - sum(total2))
-    return total1 - sum(total2)
+"""
+Given a string, turn each character into its ASCII character code and 
+join them together to create a number - let's call this number total1:
+'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+Then replace any incidence of the number 7 with the number 1, 
+and call this number 'total2':
+
+total1 = 656667
+              ^
+total2 = 656661
+              ^
+Then return the difference between the sum of the digits in total1 and total2:
+
+  (6 + 5 + 6 + 6 + 6 + 7)
+- (6 + 5 + 6 + 6 + 6 + 1)
+-------------------------
+                       6
+"""
+# def calc(x):
+#     """
+#     My solution - failed
+#     calc('dCEpBuRpGhYyM') exp: 30 :my 138
+#     """
+#     total1 = sum(ord(i) for i in x)
+#     total2 = []
+#     for i in x:
+#         ascii_str = str(ord(i))
+#         if '7' in str(ascii_str):
+#             ascii_str = ascii_str.replace('7', '1')
+#         total2.append(int(ascii_str))
+#     return total1 - sum(total2)
+
+
+def calc(s):
+    """
+    Best practices
+    """
+    total1 = ''.join(map(lambda c: str(ord(c)), s))
+    print(total1)
+    total2 = total1.replace('7', '1')
+    return sum(map(int, total1)) - sum(map(int, total2))
+
 
 calc('dCEpBuRpGhYyM')# 30
-# calc('jaam')# 12
+calc('jaam')# 12
