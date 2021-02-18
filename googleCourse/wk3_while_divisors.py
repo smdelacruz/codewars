4b2b338042acfde2cb41c316051375b78f4f2f58
Shareena de la Cruz <sdelacruz@equuscs.com>
2021-02-18 15:47:23 +0800

    2182021

--- googleCourse/wk3_while_divisors.py
+++ googleCourse/wk3_while_divisors.py
@@ -5,16 +5,16 @@ def sum_divisors(n):
     while count < n:
         if n % count == 0:
             sum += count
-            # print(sum)
+            print(sum)
         count += 1
-        # print("count ", count)
+        print("count ", count)
     return sum
 
-print(sum_divisors(0))
+# print(sum_divisors(0))
 # 0
 print(sum_divisors(3)) # Should sum of 1
 # 1
-print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
-# # 55
-print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
+# print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+# # # 55
+# print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
 # 114
--- googleCourse/wk4_assessment_3.py
+++ googleCourse/wk4_assessment_3.py
@@ -1,8 +1,14 @@
 def combine_lists(list1, list2):
-    return Jamies_list + Drews_list[::-1]
-  
+    return list2 + list1[::-1]
+
 	
 Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
 Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
 
 print(combine_lists(Jamies_list, Drews_list))
+
+
+
+['Mike', 'Carol', 'Greg', 'Marcia', 'Peter', 'Jan', 'Bobby', 'Cindy', 'Alice']
+
+
