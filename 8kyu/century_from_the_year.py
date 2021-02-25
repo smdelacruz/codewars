5b8e4b685bd577dcc68fb02aef3d1c3cc6aac821
Shareena de la Cruz <sdelacruz@equuscs.com>
2021-02-25 16:39:38 +0800

    20210225

--- 8kyu/century_from_the_year.py
+++ 8kyu/century_from_the_year.py
@@ -1,15 +1,32 @@
+"""
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+Task :
+Given a year, return the century it is in.
+
+Input , Output Examples ::
+centuryFromYear(1705)  returns (18)
+centuryFromYear(1900)  returns (19)
+centuryFromYear(1601)  returns (17)
+centuryFromYear(2000)  returns (20)
+Hope you enjoy it .. Awaiting for Best Practice Codes
+"""
+
 def century(year):
-    # unFinish 
-    pp = year / 100
-    ll = "{:.2f}".format(pp)
-    print(ll)
-    p = year // 100
-    print(p)
-    return year % 100
+    """
+    My solution: Wrong.. revealed the answer
+    """
+    return year // 100
 
 
+import math
+def century(year):
+    """
+    Best practice solution
+    """
+    return math.ceil(year // 100)
 
-print(century(1705)) #18
-# print(century(1900)) #19
-# print(century(89)) #1
-# print(century(356)) #4
+print(century(1705))
+print(century(1900))
+print(century(1601))
+print(century(2000))
