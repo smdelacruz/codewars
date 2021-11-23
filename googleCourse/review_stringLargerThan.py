cadbd05f9edcc8686b7f87f9cdbf96da45edb855
Shareena <smdelacruz28@yahoo.com>
2021-11-24 00:14:25 +0800

    Review CsDojo2

--- googleCourse/review_stringLargerThan.py
+++ googleCourse/review_stringLargerThan.py
@@ -7,3 +7,14 @@ def larger_than(a, b):
         for i in range(len(a)):
             if a[i] > b[i]: return True
     return False 
+
+
+def larger_than(a, b):
+    """
+    Correct solution
+    """
+    if len(a) > len(b): return True
+    elif len(a) == len(b):
+        for i in range(len(a)):
+            if a[i] > b[i]: return True
+    return False 
